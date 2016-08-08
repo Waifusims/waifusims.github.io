5abae5b1d351b161f17471502fd29aa3e578e692
Dennis McNamara <dmcnam2@uic.edu>
2016-08-08 15:48:47 -0500

    Skill details
    - Filled out base skill descriptions for the rest of fencer
    specialization #1
    - Removed a stray print statement

--- EOV/data/classSkills.js
+++ EOV/data/classSkills.js
@@ -286,40 +286,56 @@ var classSkills =
 			SharpThrust:
 			{
 				name_en: "Sharp Thrust",
+				name_jp: "シャープスラスト",
+				details: "Deals melee STR-based stab damage to one target. For the rest of the turn, the Fencer's chance of beign targeted and evasion will be increased. Has a 500% speed modifier and +20 base accuracy at all levels.",
 				dep: {VisionThrust:3},
 				active: true,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:2, y:5}
 			},
 			CounterBoost:
 			{
 				name_en: "Counter Boost",
+				name_jp: "カウンタブースト",
+				details: "Gives a chance to add extra attacks when Sylphid activates.",
 				dep: {Sylphid:5},
+				active: false,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:2, y:6}
 			},
 			Unburdened:
 			{
 				name_en: "Unburdened",
+				name_jp: "持たざる者",
+				details: "Increases the Fencer's evasion based on the number of empty equipment slots. The format of evasion bonuses is: 1 empty slot, 2 empty slots, 3 empty slots, no equipment.",
 				dep: {},
+				active: false,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:3, y:0}
 			},
 			PileOn:
 			{
 				name_en: "Pile On",
+				name_jp: "誘い込み",
+				details: "On this turn, when the fencer dodges an attack, all other party members in their row will counter-attack the source of the attack. The damage is based on the party members' stats, not the Fencer's. Has a 300% speed modifier at all levels.",
 				dep: {BewitchingSwordDance:3},
 				active: true,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:3, y:1}
 			},
 			AvoidAccel:
 			{
 				name_en: "Avoid Accel",
+				name_jp: "アボイドアクセラ",
+				details: "Each time the Fencer dodges an attack, the Union gauge increases.",
 				dep: {BewitchingSwordDance:3},
 				active: true,
 				reqLevel: 20,
@@ -330,19 +346,25 @@ var classSkills =
 			DivineWind:
 			{
 				name_en: "Divine Wind",
+				name_jp: "ディバインガスト",
+				details: "Increases all party members' evasion for one turn. Each time a party member dodges, the bonus provided by Divine Wind is reduced. Has a 150% speed modifier at all levels.",
 				dep: {PreemptiveCurtain:3},
 				active: true,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:3, y:4}
 			},
 			RevengeThrust:
 			{
 				name_en: "Revenge Thrust",
+				name_jp: "リベンジスラスト",
+				details: "Deals melee STR-based stab+volt damage to one enemy. Damage starts at 100%, and is multiplied by a damage bonus times the number of times the Fencer has dodged. Has a 75% speed modifier and +10 base accuracy at all levels.",
 				dep: {SharpThrust:5},
 				active: true,
 				reqLevel: 20,
 				level: 0,
+				max: 10,
 				coords: {x:3, y:5}
 			}
 		},
