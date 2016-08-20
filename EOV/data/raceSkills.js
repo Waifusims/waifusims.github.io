336a53c01d5fd2d3ea5d6ae30de8812029d658df
Dennis McNamara <dmcnam2@uic.edu>
2016-08-20 12:01:01 -0500

    missing powershot info fix and sitemap

---
+++ EOV/data/raceSkills.js
@@ -0,0 +1,306 @@
+var raceSkills = 
+{
+	earthrun:
+	{
+		Analyze:
+		{
+			name_en: "Analyze",
+			name_jp: "アナライズ",
+			details: "Temporarily register a single monster's data.<br>Cost: 1",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 0,
+			max: 0,
+			coords: {x:0, y:0}
+		},
+		DoubleAttack:
+		{
+			name_en: "Double Attack",
+			name_jp: "ダブルアタック",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 0,
+			max: 0,
+			coords: {x:1, y:0}
+		},
+		FullSpeedEscape:
+		{
+			name_en: "Full Speed Escape",
+			name_jp: "全力逃走",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 0,
+			max: 0,
+			coords: {x:0, y:1}
+		},
+		MagicHeal:
+		{
+			name_en: "Magic Heal",
+			name_jp: "マジックヒール",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 0,
+			max: 0,
+			coords: {x:1, y:1}
+		},
+		LightofBlessing:
+		{
+			name_en: "Light of Blessing",
+			name_jp: "祝福の光",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 0,
+			max: 1,
+			coords: {x:0, y:2}
+		},
+		GraceoftheEarth:
+		{
+			name_en: "Grace of the Earth",
+			name_jp: "大地の恩寵",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 0,
+			max: 1,
+			coords: {x:1, y:2}
+		},
+		Fishing:
+		{
+			name_en: "Fishing",
+			name_jp: "フィッシング",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 0,
+			max: 1,
+			coords: {x:0, y:3}
+		},
+		WoodlandSearchSkills:
+		{
+			name_en: "Woodland Search Skills",
+			name_jp: "樹海探索術",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 0,
+			max: 1,
+			coords: {x:1, y:3}
+		},
+		BasicResuscitation:
+		{
+			name_en: "Basic Resuscitation",
+			name_jp: "簡易蘇生",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 5,
+			max: 1,
+			coords: {x:0, y:4}
+		},
+		OrganizationalSkills:
+		{
+			name_en: "Organizational Skills",
+			name_jp: "整頓術",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 5,
+			max: 1,
+			coords: {x:1, y:4}
+		},
+		WoodlandGatheringSkills:
+		{
+			name_en: "Woodland Gathering Skills",
+			name_jp: "樹海採集術",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 5,
+			max: 1,
+			coords: {x:0, y:5}
+		},
+		AnimalHusbandry:
+		{
+			name_en: "Animal Husbandry",
+			name_jp: "飼育術",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 5,
+			max: 1,
+			coords: {x:1, y:5}
+		},
+		TriShield:
+		{
+			name_en: "Tri-Shield",
+			name_jp: "トライシールド",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 10,
+			max: 1,
+			coords: {x:0, y:6}
+		},
+		MartialArts:
+		{
+			name_en: "Martial Arts",
+			name_jp: "体術",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 10,
+			max: 1,
+			coords: {x:1, y:6}
+		},
+		ServingPower:
+		{
+			name_en: "Serving Power",
+			name_jp: "セービングパワー",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 10,
+			max: 1,
+			coords: {x:0, y:7}
+		},
+		ArmorMastery:
+		{
+			name_en: "Armor Mastery",
+			name_jp: "重装マスタリー",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 10,
+			max: 1,
+			coords: {x:1, y:7}
+		},
+		BlackMist:
+		{
+			name_en: "Black Mist",
+			name_jp: "黒霧",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 15,
+			max: 1,
+			coords: {x:0, y:8}
+		},
+		BodyImprovement:
+		{
+			name_en: "Body Improvement",
+			name_jp: "肉体強化",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 15,
+			max: 1,
+			coords: {x:1, y:8}
+		},
+		AthleticTraining:
+		{
+			name_en: "Athletic Training",
+			name_jp: "走力鍛錬",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 15,
+			max: 1,
+			coords: {x:0, y:9}
+		},
+		SelfStudy:
+		{
+			name_en: "Self Study",
+			name_jp: "自学自習",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 15,
+			max: 1,
+			coords: {x:1, y:9}
+		},
+		IndomitableSpirit:
+		{
+			name_en: "Indomitable Spirit",
+			name_jp: "不屈の闘志",
+			details: "",
+			dep: {},
+			active: true,
+			level: 0,
+			reqLevel: 20,
+			max: 1,
+			coords: {x:0, y:10}
+		},
+		EarthsFavor:
+		{
+			name_en: "Earth's Favor",
+			name_jp: "大地の寵愛",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 20,
+			max: 1,
+			coords: {x:1, y:10}
+		},
+		Anatomy:
+		{
+			name_en: "Anatomy",
+			name_jp: "解剖学",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 20,
+			max: 1,
+			coords: {x:0, y:11}
+		},
+		Gourmet:
+		{
+			name_en: "Gourmet",
+			name_jp: "グルメ",
+			details: "",
+			dep: {},
+			active: false,
+			level: 0,
+			reqLevel: 20,
+			max: 1,
+			coords: {x:1, y:11}
+		}
+	},
+	lunaria:
+	{
+
+	},
+	therian:
+	{
+
+	},
+	brownie:
+	{
+
+	}
+}
