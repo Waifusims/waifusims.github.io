d1b815db9f040b88840363cb3a9b11d48d63811e
stapler <psidefenseup@gmail.com>
2025-02-17 14:56:37 -0600

    init github pages workflow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath:"/waifusims.github.io",
 };
 
 export default nextConfig;
