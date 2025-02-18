e7156d61fd7c34839baf649e36735cb5373644e6
stapler <psidefenseup@gmail.com>
2025-02-17 18:14:32 -0600

    add case-insenstive routing, fix homepage

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath:"/waifusims.github.io",
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/index.html",
+    },
+  ],
 };
 
 export default nextConfig;
